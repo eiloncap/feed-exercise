0e39d9a0d1e48fc9384d54d36b55b1355ecfc491
Eilon <eilon@lightricks.com>
2022-07-17 14:36:46 +0300

    Part 2: Fetch FeedItems.

--- app/src/main/java/com/lightricks/feedexercise/ui/feed/FeedViewModel.kt
+++ app/src/main/java/com/lightricks/feedexercise/ui/feed/FeedViewModel.kt
@@ -1,9 +1,14 @@
 package com.lightricks.feedexercise.ui.feed
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.*
 import com.lightricks.feedexercise.data.FeedItem
+import com.lightricks.feedexercise.network.FeedApiService
+import com.lightricks.feedexercise.network.FeedApiServiceProvider
+import com.lightricks.feedexercise.network.GetFeedResponse
 import com.lightricks.feedexercise.util.Event
-import java.lang.IllegalArgumentException
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 /**
  * This view model manages the data for [FeedFragment].
@@ -13,6 +18,7 @@ open class FeedViewModel : ViewModel() {
     private val isEmpty = MutableLiveData<Boolean>()
     private val feedItems = MediatorLiveData<List<FeedItem>>()
     private val networkErrorEvent = MutableLiveData<Event<String>>()
+    private val feedApiService: FeedApiService = FeedApiServiceProvider.getFeedApiService()
 
     fun getIsLoading(): LiveData<Boolean> = isLoading
     fun getIsEmpty(): LiveData<Boolean> = isEmpty
@@ -23,10 +29,38 @@ open class FeedViewModel : ViewModel() {
         refresh()
     }
 
+    @SuppressLint("CheckResult")
     fun refresh() {
-        //todo: fix the implementation
+        isLoading.value = true
+        feedApiService.getFeed()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ feedResponse ->
+                handleResponse(feedResponse)
+            }, { error ->
+                handleNetworkError(error)
+            })
+    }
+
+    private fun handleResponse(feedResponse: GetFeedResponse) {
+        feedResponse.templatesMetadata.map {
+            FeedItem(
+                it.id,
+                FeedApiServiceProvider.thumbnailURIPrefix + it.templateThumbnailURI,
+                it.isPremium
+            )
+        }.toList()
+            .let {
+                isLoading.value = false
+                isEmpty.value = it.isEmpty()
+                feedItems.value = it
+            }
+    }
+
+    private fun handleNetworkError(error: Throwable?) {
         isLoading.value = false
         isEmpty.value = true
+        networkErrorEvent.value = Event(error?.message ?: "")
     }
 }
 
